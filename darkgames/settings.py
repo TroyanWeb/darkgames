b84eff083bd5a8ef232c9a2e400a251a2cd0bc8d
Сергей Сушков <sereginnsk7755@gmail.com>
2026-02-19 03:58:55 +0700

    Fix staticfiles config and add Procfile

--- darkgames/settings.py
+++ darkgames/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+ALLOWED_HOSTS = ['*']  # временно для теста
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
